d724e2f4bdd6b4f556ae9b6e2161ce0ae78d689c
Sharovskaya Vlada <bonik.sweden227@gmail.com>
2024-05-22 17:03:43 +0300

    Deleted files, renamed main method

--- src/main/java/CoffeeMain.java
+++
@@ -1,8 +0,0 @@
-public class CoffeeMain {
-    public static void main(String[] args) {
-        TurningOn on = new TurningOn(true);
-        TurningOn off = new TurningOn(false);
-
-        System.out.println("Coffee machine is turned on: " + on.isTurnOn());
-    }
-}
