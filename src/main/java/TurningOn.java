31050ecc18d61674fd1496c6cc5c2a5e2cb1dddb
Sharovskaya Vlada <bonik.sweden227@gmail.com>
2024-05-22 14:21:49 +0300

    first commit

---
+++ src/main/java/TurningOn.java
@@ -0,0 +1,12 @@
+public class TurningOn {
+
+    private boolean turnOn;
+
+    public TurningOn(boolean turnOn) {
+        this.turnOn = turnOn;
+    }
+
+    public boolean isTurnOn() {
+        return turnOn;
+    }
+}
